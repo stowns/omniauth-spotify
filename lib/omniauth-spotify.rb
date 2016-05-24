b2e1baf427c331aea6f3b2f0fcd2936ffbe642a9
Hannah Howard <hannah@hannahhoward.net>
2016-05-24 16:06:47 -0700

    fix(oauth2): override callback url to not include query string

--- lib/omniauth-spotify.rb
+++ lib/omniauth-spotify.rb
@@ -50,6 +50,11 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('me').parsed
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
     end
   end
 end
